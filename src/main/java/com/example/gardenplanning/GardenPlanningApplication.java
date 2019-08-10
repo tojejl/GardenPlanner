61f02af966276df58c7201ef13e942ab6a0dba9c
jennifer toje <Jennifer>
2019-08-10 15:33:31 -0500

    added garden controller and second page to localhost:8080

--- src/main/java/com/example/gardenplanning/GardenPlanningApplication.java
+++ src/main/java/com/example/gardenplanning/GardenPlanningApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GardenPlanningApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GardenPlanningApplication.class, args);
 	}
 
