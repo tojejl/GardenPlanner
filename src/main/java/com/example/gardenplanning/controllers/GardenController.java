bf27bb7a478738b4552d3caae1edf2d16add410a
jennifer toje <Jennifer>
2019-08-13 14:05:09 -0500

    Added plants.html and a form to store data in the plants.html page

--- src/main/java/com/example/gardenplanning/controllers/GardenController.java
+++ src/main/java/com/example/gardenplanning/controllers/GardenController.java
@@ -1,18 +1,42 @@
 package com.example.gardenplanning.controllers;
 
+import com.example.gardenplanning.model.Plant;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
-@RequestMapping("garden")
+@RequestMapping("plants")
 public class GardenController {
 
-    @RequestMapping()
+    private static List<Plant> plants;
 
-    public String loadGardenController() {
-        return "index.html";
+    public GardenController() {
+        plants = new ArrayList<Plant>();
+        plants.add(new Plant("Basil"));
+        plants.add(new Plant("Cucumber"));
     }
 
+    @RequestMapping("")
+    public String loadplants(Model model) {
+        System.out.println("Loading Plants");
 
+        model.addAttribute("plants", plants);
+        model.addAttribute("count", 42);
+        return "plants";
+    }
+
+    @RequestMapping("new")
+    public String addplants(Model model, @RequestParam String plantType) {
+        System.out.println("Adding plant: " + plantType);
+
+        plants.add(new Plant(plantType));
+        return "redirect:";
+
+    }
 }
---
+++ src/main/java/com/example/gardenplanning/model/Plant.java
@@ -0,0 +1,23 @@
+package com.example.gardenplanning.model;
+
+public class Plant {
+    private String vegetable;
+    //private String herb;
+
+
+
+    public Plant (String vegetable){
+        this.vegetable = vegetable;
+        //this.herb = herb;
+
+    }
+
+    //public String getHerb() {
+        //return herb;
+    //}
+
+
+    public String getVegetable() {
+        return vegetable;
+    }
+}
